248e4b733def2c5103ff8629a46603b0d9354676
Sebastian Winkler <sekl@slmn.de>
2016-11-02 16:00:50 +0100

    introduces concurrency

--- main.go
+++ main.go
@@ -28,6 +28,11 @@ var (
 	SafeLinksFilename string
 )
 
+const (
+	MaxConcurrentBoardRequests   = 2
+	MaxConcurrentArticleRequests = 4
+)
+
 func stringAlreadyInFile(filename string, needle string) bool {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -116,7 +121,7 @@ func main() {
 	}
 
 	RegexpUrl, err = regexp.Compile(
-		`(http:\/\/[a-z0-9]+.dcinside.com\/(viewimage(M)(Pop)?.php\\?[^"\'\<\ ]+|[a-z0-9\-\_]+\.(jpg|jpeg|png)))`)
+		`(http:\/\/[a-z0-9]+.dcinside.com\/(viewimage(M)?(Pop)?.php\\?[^"\'\<\ ]+|[a-z0-9\-\_]+\.(jpg|jpeg|png)))`)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -124,64 +129,111 @@ func main() {
 
 	BoardList = cfg.Section("general").Key("boards").Strings(",")
 	TargetFolder = cfg.Section("general").Key("feedfolder").String()
+
+	semCreateFeeds := make(chan bool, MaxConcurrentBoardRequests)
 	for _, board := range BoardList {
+		semCreateFeeds <- true
 		log.Printf("checking board: %s", board)
-		URL := "http://gall.dcinside.com/board/lists/?id=" + board
+		go createFeedForBoard(semCreateFeeds, board)
+	}
+	for i := 0; i < cap(semCreateFeeds); i++ {
+		semCreateFeeds <- true
+	}
+}
 
-		now := time.Now()
-		feed := &Feed{
-			Title:       "dcinside: " + board,
-			Link:        &Link{Href: URL},
-			Description: "first page of the best articles on the dcinside board " + board,
-			Created:     now,
+func createFeedForBoard(semCreateFeeds <-chan bool, board string) {
+	defer func() { <-semCreateFeeds }()
+
+	URL := "http://gall.dcinside.com/board/lists/?id=" + board
+	now := time.Now()
+	feed := &Feed{
+		Title:       "dcinside: " + board,
+		Link:        &Link{Href: URL},
+		Description: "first page of the best articles on the dcinside board " + board,
+		Created:     now,
+	}
+
+	list, err := goinside.FetchBestList(URL, 1)
+	if err != nil {
+		log.Fatalln("FetchList:", err)
+	}
+
+	semArticleToFeed := make(chan bool, MaxConcurrentArticleRequests)
+	for _, item := range list.Items {
+		semArticleToFeed <- true
+		go addArticleToFeed(semArticleToFeed, item, feed)
+	}
+	for i := 0; i < cap(semArticleToFeed); i++ {
+		semArticleToFeed <- true
+	}
+
+	atom, err := feed.ToRss()
+	if err != nil {
+		log.Fatal(err)
+	}
+	targetXml := TargetFolder + string(filepath.Separator) + board + ".xml"
+	log.Printf("writing %s feed to: %s", board, targetXml)
+	err = ioutil.WriteFile(targetXml, []byte(atom), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addArticleToFeed(semArticleToFeed <-chan bool, item *goinside.ListItem, feed *Feed) {
+	defer func() { <-semArticleToFeed }()
+
+	cArticle := make(chan *goinside.Article)
+	cImageUrls := make(chan []string)
+
+	go fetchArticle(cArticle, item)
+	go fetchImageUrls(cImageUrls, item)
+
+	var article *goinside.Article
+	var imageUrls []string
+	for i := 0; i <= 1; i++ {
+		select {
+		case article = <-cArticle:
+		case imageUrls = <-cImageUrls:
 		}
+	}
 
-		list, err := goinside.FetchBestList(URL, 1)
-		if err != nil {
-			log.Fatalln("FetchList:", err)
+	content := ""
+	if len(imageUrls) > 0 {
+		content += "<p><b>Embedded images:</b><br />"
+		for _, imageUrl := range imageUrls {
+			content += fmt.Sprintf("<a href=\"%s\" target=\"_blank\"><img src=\"%s\" /><br /></a>", imageUrl, imageUrl)
 		}
-		for _, item := range list.Items {
-			article, err := item.Fetch()
-			if err != nil {
-				log.Fatal(err)
-			}
-			var imageUrls []string
-			if article.HasImage {
-				imageUrls, err = item.FetchImageURLs()
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			content := ""
-			if len(imageUrls) > 0 {
-				content += "<p><b>Embedded images:</b><br />"
-				for _, imageUrl := range imageUrls {
-					content += fmt.Sprintf("<img src=\"%s\" /><br />", imageUrl)
-				}
-				content += "</p>"
-			}
-			content += article.Content
-			editedContent := html.UnescapeString(string(RegexpUrl.ReplaceAllFunc([]byte(html.UnescapeString(content)), imageProxyUrl)))
+		content += "</p>"
+	}
+	content += article.Content
+	editedContent := html.UnescapeString(string(RegexpUrl.ReplaceAllFunc([]byte(html.UnescapeString(content)), imageProxyUrl)))
 
-			feed.Add(&Item{
-				Title:       article.Subject,
-				Link:        &Link{Href: article.URL},
-				Description: string(html.UnescapeString(editedContent)),
-				Author:      &Author{Name: article.Name},
-				Created:     article.Date,
-				Id:          article.URL,
-			})
-		}
+	feed.Add(&Item{
+		Title:       article.Subject,
+		Link:        &Link{Href: article.URL},
+		Description: string(html.UnescapeString(editedContent)),
+		Author:      &Author{Name: article.Name},
+		Created:     article.Date,
+		Id:          article.URL,
+	})
+}
 
-		atom, err := feed.ToRss()
+func fetchArticle(channel chan<- *goinside.Article, item *goinside.ListItem) {
+	article, err := item.Fetch()
+	if err != nil {
+		log.Fatal(err)
+	}
+	channel <- article
+}
+
+func fetchImageUrls(channel chan<- []string, item *goinside.ListItem) {
+	if item.HasImage {
+		imageUrls, err := item.FetchImageURLs()
 		if err != nil {
 			log.Fatal(err)
 		}
-		targetXml := TargetFolder + string(filepath.Separator) + board + ".xml"
-		log.Printf("writing %s feed to: %s", board, targetXml)
-		err = ioutil.WriteFile(targetXml, []byte(atom), 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
+		channel <- imageUrls
+	} else {
+		channel <- make([]string, 0)
 	}
 }
